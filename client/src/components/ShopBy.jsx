b5ea9e9cddbdac173b5d14618e3f4f417bb9198b
Malay <malay.conceptive@gmail.com>
2026-01-19 20:32:40 +0530

    Task-1 done

--- client/src/components/ShopBy.jsx
+++ client/src/components/ShopBy.jsx
@@ -2,7 +2,14 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import HorSlider from "./HorSlider";
 
-const ShopBy = ({ filter, title }) => {
+ const data = [
+    { src: "/GenInfo/adidas.jpg", name: "Adidas", to: "/search/adidas" },
+    { src: "/GenInfo/nike.png", name: "Nike", to: "/search/nike" },
+    { src: "/GenInfo/skechers.jpg", name: "Skechers", to: "/search/skechers" },
+    { src: "/GenInfo/puma.jpg", name: "Puma", to: "/search/puma" },
+  ]; 
+
+const ShopBy = ({ filter, title, isBestSeller }) => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -34,23 +41,34 @@ const ShopBy = ({ filter, title }) => {
 
   return (
     <>
-      <div className="mt-10 mb-2 text-2xl">{title}</div>
-      <div className="overflow-x-auto overflow-y-hidden md:max-w-full scroll-container mb-10 mx-auto relative scroll-container">
-        {loading && <p>Loading...</p>}
-        {error && <p>Error while fetching: {error.message}</p>}
-
-        <div className="flex flex-nowrap space-x-4">
-          {/* Ensure products is always an array */}
-          {(Array.isArray(products) ? products : []).map((elem) => (
-            <HorSlider
-              product={elem}
-              key={elem._id || elem.id} // fallback if _id is missing
-              className="inline-block"
-              home={true}
+       <div className="flex flex-col items-center my-16 w-full">
+      <p className="prose prose-2xl font-bold mb-6">{title}</p>
+   {isBestSeller &&   <div className="flex flex-wrap justify-center">
+        {data.map((elem, id) => (
+          <div
+            key={id}
+            className="relative w-[340px] h-[340px] mx-2 mb-6 hover:text-white"
+          >
+            <div className="absolute w-full flex justify-center items-center top-4  ">
+              <p className="logo font-semibold z-50 ">{elem.name}</p>
+            </div>
+            <img
+              src={elem.src}
+              alt={elem.name}
+              className="w-full h-full object-cover"
             />
-          ))}
-        </div>
-      </div>
+            <button
+              onClick={() => navigate(elem.to)}
+              className="absolute inset-0 flex items-center justify-center
+                             bg-gray-800 text-white opacity-0 hover:opacity-80 transition-opacity duration-200"
+            >
+              Explore →
+            </button>
+          </div>
+        ))}
+      </div>}
+    </div>
+      
     </>
   );
 };
